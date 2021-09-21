652f45c923ef5f3d59f937f53b141f3babcd3962
tamasstephen <kicsitnagyon@gmail.com>
2021-09-21 12:19:43 +0200

    feat: Add status bar

--- modules/gameMod.js
+++ modules/gameMod.js
@@ -1,68 +1,74 @@
-import { util } from "./util.js";
-import { createEndScreen, removeElement } from "./createBoard.js";
-export { Game }
+import { util } from './util.js';
+import { createEndScreen, removeElement } from './createBoard.js';
+export { Game };
 
 class Game {
-    constructor(gameData, timer=20){
-        this.gameData = gameData;
-        this.totalQuestionNumber = this.gameData.length;
-        this.timeLimit = this.timeLimit;
-        this.rightAnswer = this.rightAnswer;
-        this.score = 0;
-        this.h2 = null;
-        this.answerContainers = null;
-        this.handleClick = util.clickHandlerWrapper(this);
-    }
+	constructor(gameData, timer = 20) {
+		this.gameData = gameData;
+		this.totalQuestionNumber = this.gameData.length;
+		this.timeLimit = this.timeLimit;
+		this.rightAnswer = this.rightAnswer;
+		this.score = 0;
+		this.h2 = null;
+		this.answerContainers = null;
+		this.handleClick = util.clickHandlerWrapper(this);
+	}
+
+	handleScore(answer) {
+		answer === this.rightAnswer ? (this.score += 100) : (this.score -= 50);
+	}
+
+	provideNewQuestions() {
+		const currentData = this.gameData.shift();
+		this.h2.textContent = currentData.question;
+		this.fillAnswers(currentData.answers);
+		this.answerContainers.forEach((container) => container.addEventListener('click', this.handleClick));
+		this.setStatusbar();
+	}
+
+	fillAnswers(answers) {
+		let i = 0;
+		for (const container of this.answerContainers) {
+			container.querySelector('.answer').textContent = answers[i].answer;
+			container.querySelector('.question-number').textContent = i + 1;
+			this.saveRightAnswer(answers[i]);
+			i++;
+		}
+	}
+
+	setStatusbar() {
+		const statusBar = document.querySelector('.status-inner');
+		const precentToAdd = 1 / this.totalQuestionNumber * 100;
+		const startPercentage = Number(statusBar.style.width.replace('%', ''));
+		const currentLength = this.totalQuestionNumber - this.gameData.length;
+		document.querySelector('#text-status').textContent = `Questions: ${currentLength}/${this.totalQuestionNumber}`;
+		statusBar.style.width = String(startPercentage + precentToAdd) + "%";
+	}
+
+	saveRightAnswer(answer) {
+		if (answer.right) {
+			this.rightAnswer = answer.answer;
+		}
+	}
+
+	checkQuestionsLeft() {
+		return this.gameData.length === 0 ? true : false;
+	}
+
+	endgame() {
+		const elementToRemove = document.querySelector('#playfield');
+		removeElement(elementToRemove);
+		const endScreen = createEndScreen();
+		endScreen.querySelector('p:first-child').textContent = 'Your score is:';
+		endScreen.querySelector('p:last-child').textContent = this.score;
+		document.querySelector('.wrapper').appendChild(endScreen);
+	}
 
 
-    handleScore(answer){
-        answer === this.rightAnswer ? this.score += 100 : this.score -= 50;
-    }
-
-    provideNewQuestions(){
-        const currentData = this.gameData.shift();
-        this.h2.textContent = currentData.question;
-        this.fillAnswers(currentData.answers);
-        this.answerContainers.forEach(container => container.addEventListener('click', this.handleClick));
-    }
-
-    fillAnswers(answers){
-        let i = 0; 
-        for (const container of this.answerContainers) {
-            container.querySelector(".answer").textContent = answers[i].answer;
-            container.querySelector(".question-number").textContent = i + 1;
-            this.saveRightAnswer(answers[i]);
-            i++;
-       }
-    }
-
-    setStatusbar(){
-        const currentLength = this.totalQuestionNumber -  this.gameData.length;
-    }
-
-    saveRightAnswer(answer){
-        if (answer.right){
-            this.rightAnswer = answer.answer;
-        }
-    }
-
-    checkQuestionsLeft(){
-        return this.gameData.length === 0 ? true : false;
-    }
-
-    endgame(){
-        const elementToRemove = document.querySelector("#playfield");
-        removeElement(elementToRemove);
-        const endScreen = createEndScreen();
-        endScreen.querySelector("p:first-child").textContent = "Your score is:";
-        endScreen.querySelector("p:last-child").textContent = this.score;
-        document.querySelector(".wrapper").appendChild(endScreen);
-    }
-
-    init(){
-        this.h2 = document.querySelector("#playfield > h2");
-        this.answerContainers = document.querySelectorAll(".answer-container");
-        this.provideNewQuestions();
-    }
-
+	init() {
+		this.h2 = document.querySelector('#playfield > h2');
+		this.answerContainers = document.querySelectorAll('.answer-container');
+		document.querySelector('.status-inner').style.width = '1%';
+		this.provideNewQuestions();
+	}
 }
