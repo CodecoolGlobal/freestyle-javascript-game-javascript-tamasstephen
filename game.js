c34c6d28b71299f803dfdce27726484517791901
matekaszas13 <matekaszas17@gmail.com>
2021-09-22 20:18:12 +0200

    refactor: change class to delete all menu buttons

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ import {createMenuButtons} from "./modules/menu.js";
 
 function initGame() {
     const board = createBoard();
-    const elementToRemove = document.querySelector('.menuBtn');
+    const elementToRemove = document.querySelector('.buttons');
     document.querySelector(".wrapper").appendChild(board);
 
     removeElement(elementToRemove);
